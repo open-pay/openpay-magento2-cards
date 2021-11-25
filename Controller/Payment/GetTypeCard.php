edaca91c4a5811188e0f34897dc0de3ec5b3c636
Jose Guadalupe Romero <jose.romero@openpay.mx>
2021-11-25 11:59:20 -0600

    Integración de consulta de Bines México/Colombia

--- Controller/Payment/GetTypeCard.php
+++ Controller/Payment/GetTypeCard.php
@@ -41,17 +41,58 @@ class GetTypeCard extends \Magento\Framework\App\Action\Action{
 
         try {
             $this->logger->debug('#CardBin', array('cardInfo' => $post['card_bin']));
-            $cardInfo = $openpay->bines->get($post['card_bin']);
-            
-            $data = $cardInfo->type;
 
+            $country = $this->payment->getCountry();
+            if($country == 'MX'){
+                $openpay = $this->payment->getOpenpayInstance();
+                $cardInfo = $openpay->bines->get($post['card_bin']);
+                $data = array(
+                    'status' => 'success',
+                    'card_type' => $cardInfo->type
+                );
+            } else if($country == 'CO') {
+                $cardInfo = $this->requestOpenpay('/cards/validate-bin?bin='.$post['card_bin'], $country, $this->payment->isSandbox(), "GET");
+                $data = array(
+                    'status' => 'success',
+                    'card_type' => $cardInfo->card_type
+                );
+            }
         } catch (\Exception $e) {
-            $this->logger->error('#ErrorBin', array('msg' => $e->getMessage()));                    
+            $this->logger->error('#ErrorBin', array('msg' => $e->getMessage()));
+            $data = array(
+                'status' => 'error',
+                'card_type' => "credit card not found"
+            );                   
         }
 
         $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
         $resultJson->setData($data);
         return $resultJson;
+    }
+
+    public function requestOpenpay($api, $is_sandbox, $method = 'GET') {
+        $url = 'https://api.openpay.co/v1';
+        $sandbox_url = 'https://sandbox-api.openpay.co/v1';
+    
+        $absUrl = $is_sandbox ? $sandbox_url : $url;
+        $absUrl .= $api;
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $absUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        $result = curl_exec($ch);
+    
+        if (curl_exec($ch) === false) {
+            $this->logger->error("Curl error", array("curl_errno" => curl_errno($ch), "curl_error" => curl_error($ch)));
+        } else {
+            $info = curl_getinfo($ch);
+            $this->logger->debug("requestOpenpay", array("HTTP code " => $info['http_code'], "on request to" => $info['url']));
+        }
+    
+        curl_close($ch);
+    
+        return json_decode($result);
     }
 
 }
