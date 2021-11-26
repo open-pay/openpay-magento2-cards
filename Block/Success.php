e03119d953b3ba5da27c95d9a089adab86077b67
Julio Cesar Vazquez Cervantes <julio.vazquez@openpay.mx>
2021-11-26 13:16:30 -0600

    Manejo de errores al momento de fallar 3D-Secure

--- Block/Success.php
+++ Block/Success.php
@@ -4,5 +4,20 @@ namespace Openpay\Cards\Block;
 
 class Success extends \Magento\Framework\View\Element\Template
 {
+    protected $context;
+    protected $coreRegistry;
+
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        \Magento\Framework\Registry $coreRegistry
+    ) {
+        $this->coreRegistry = $coreRegistry;
+        parent::__construct($context);
+    }
+
+
+    public function getMessageError(){
+        return $this->coreRegistry->registry('messageError');
+    }
 
 }
